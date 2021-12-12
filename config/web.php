0f7d882c2ea71dcff4ce2c3c9823895729e19843
unknown <rusakdeveloper@gmail.com>
2021-12-12 12:34:33 +0300

    first commit

---
+++ config/web.php
@@ -0,0 +1,9 @@
+<?php 
+
+return  
+// необходимые настройки
+$config = [
+  'id'=> 'video',
+  'basePath' => realpath(__DIR__ . '/../')
+];
+
--- web/index.php
+++ web/index.php
@@ -1,1 +1,9 @@
-hello 
+<?php
+//  импорт yii
+  require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+  $config = require __DIR__ . '/../config/web.php';
+  // запуск приложения
+  $yii = new yii\web\Application($config);
+  $yii->run ();
+  // **другой вариант заупска 
+  // (new yii\web\Application($config))->run();
