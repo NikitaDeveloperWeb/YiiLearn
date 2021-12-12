11de47a67c33d6bf6e3beaaacc5297869093388b
unknown <rusakdeveloper@gmail.com>
2021-12-12 12:50:42 +0300

    create controllers

---
+++ controllers/SiteController.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\controllers;
+// import
+use yii\web\Controller;
+
+class SiteController extends Controller 
+{
+  public function actionIndex()
+  {
+    return 'video School';
+  }
+}
